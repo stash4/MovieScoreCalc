c1ac64e933b09c50c10820df5658804449fb8042
stash4 <high.mackey@gmail.com>
2018-01-10 03:38:33 +0900

    Update moviescores.py

--- moviescores.py
+++ moviescores.py
@@ -40,24 +40,29 @@ def eiga_com(movie_name):
     '''
     映画.comから作品のタイトル、公開日、スコア、レビュー数を取得する。
     '''
-    res = get_page(f'{movie_name} 作品情報', sites['EIGA'])
+    try:
+        res = get_page(f'{movie_name} 作品情報', sites['EIGA'])
 
-    soup = BeautifulSoup(res.text, 'html.parser')
-    # タイトル
-    name = soup.find(itemprop='name').string
-    # 公開日
-    date = soup.find(itemprop='datePublished').get('content')
-    # レビュースコア
-    rating = soup.find(itemprop='ratingValue').string
-    # レビュー数
-    count = soup.find(itemprop='reviewCount').string
+        soup = BeautifulSoup(res.text, 'html.parser')
+        # タイトル
+        name = soup.find(itemprop='name').string
+        # 公開日
+        date = soup.find(itemprop='datePublished').get('content')
+        # レビュースコア
+        rating = soup.find(itemprop='ratingValue').string
+        # レビュー数
+        count = soup.find(itemprop='reviewCount').string
 
-    ret = {
-        'name': name,
-        'date': date,
-        'rating': float(rating),
-        'count': int(count)
-    }
+        ret = {
+            'name': name,
+            'date': date,
+            'rating': float(rating),
+            'count': int(count)
+        }
+    except Exception as e:
+        print(e.args)
+        print(e.message)
+        ret = {}
     return ret
 
 
@@ -65,20 +70,26 @@ def movies_yahoo(movie_name):
     '''
     Yahoo!映画から作品のスコア、レビュー数を取得する。
     '''
-    res = get_page(f'{movie_name} 作品', sites['YAHOO'])
+    try:
+        pass
+        res = get_page(f'{movie_name} 作品', sites['YAHOO'])
 
-    soup = BeautifulSoup(res.text, 'html.parser')
-    # レビュースコア
-    rating = soup.find(itemprop='ratingValue').string
-    count = soup.find(class_='rating-score').find(class_='text-xsmall').string
-    # レビュー数
-    pattern = r'\d+'
-    count = re.search(pattern, count).group(0)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        # レビュースコア
+        rating = soup.find(itemprop='ratingValue').string
+        count = soup.find(class_='rating-score').find(class_='text-xsmall').string
+        # レビュー数
+        pattern = r'\d+'
+        count = re.search(pattern, count).group(0)
 
-    ret = {
-        'rating': float(rating),
-        'count': int(count)
-    }
+        ret = {
+            'rating': float(rating),
+            'count': int(count)
+        }
+    except Exception as e:
+        print(e.args)
+        print(e.message)
+        ret = {}
     return ret
 
 
@@ -86,16 +97,21 @@ def filmarks_com(movie_name):
     '''
     Filmarksから作品のスコア、レビュー数を取得する。
     '''
-    res = get_page(f'{movie_name} 映画情報', sites['FILMARKS'])
+    try:
+        res = get_page(f'{movie_name} 映画情報', sites['FILMARKS'])
 
-    soup = BeautifulSoup(res.text, 'html.parser')
-    # レビュースコアとレビュー数
-    desc = soup.find(property='og:description').get('content')
-    pattern = r'\d+(?:\.\d+)?'
-    ratings = re.findall(pattern, desc)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        # レビュースコアとレビュー数
+        desc = soup.find(property='og:description').get('content')
+        pattern = r'\d+(?:\.\d+)?'
+        ratings = re.findall(pattern, desc)
 
-    ret = {
-        'rating': float(ratings[1]),
-        'count': int(ratings[0])
-    }
+        ret = {
+            'rating': float(ratings[1]),
+            'count': int(ratings[0])
+        }
+    except Exception as e:
+        print(e.args)
+        print(e.message)
+        ret = {}
     return ret
