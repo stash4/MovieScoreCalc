e5b19de92b15b354d33ac985592e4727050b4b08
stash4 <high.mackey@gmail.com>
2018-01-10 12:59:39 +0900

    Update movielist.py to get image url of movie

--- movielist.py
+++ movielist.py
@@ -14,12 +14,17 @@ def movie_list():
     for movie in showing['data']:
         name = unicodedata.normalize('NFKC', movie['name'])
         desc, name_en = tohocinemas.movie_desc(movie['mcode'])
+        im_url = tohocinemas.movie_image_url(movie)
         if '<br>' in desc:
             if '正式タイトル：' in desc:
                 name, desc = desc.split('正式タイトル：')[1].split('<br>')
             else:
                 desc = desc.split('<br>')[1]
-        movies.append({'name': name, 'desc': desc, 'name_en': name_en})
+        movies.append({
+            'name': name,
+            'desc': desc,
+            'name_en': name_en,
+            'image': im_url})
 
     return movies
 
